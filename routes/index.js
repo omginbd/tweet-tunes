2a80fbf6c1b52078f0328b4416209fcd234ef8eb
Austin Hewlett <aust.hewlett@gmail.com>
2017-03-28 18:19:05 -0600

    add route comments

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
+
+/* GET Tweets */
+router.get('/tweets', function (req, res, next) {
+    res.render('tweets');
+});
+
+/* EMBED Tweet */
+
+
+/* Persist Tweet Tune */
+
+
+/* History */
+
 
 module.exports = router;
